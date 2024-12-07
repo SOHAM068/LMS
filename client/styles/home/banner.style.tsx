570ced87ba0e5c3b32fba00a8d6bef0f48a715ba
SOHAM068 <sg3533@srmist.edu.in>
2024-12-07 11:43:23 +0530

    Changes in Width in ActiveDot

--- client/styles/home/banner.style.tsx
+++ client/styles/home/banner.style.tsx
@@ -61,7 +61,7 @@ export const styles = StyleSheet.create({
 
   activeDot: {
     backgroundColor: "#2467EC",
-    width: 17, // Slightly larger for better visibility
+    width: 16.9, // Slightly larger for better visibility
     height: 10,
     borderRadius: 6,
     marginHorizontal: 3,
